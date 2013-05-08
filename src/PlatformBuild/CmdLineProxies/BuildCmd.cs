b8009ede899f8f814cfe10b435600666433dce3d
Iain Ballard <iain.ballard@7digital.com>
2013-05-08 15:59:06 +0100

    Hard coding sql to use sqlcmd against sqlexpress

--- src/PlatformBuild/CmdLineProxies/BuildCmd.cs
+++ src/PlatformBuild/CmdLineProxies/BuildCmd.cs
@@ -7,8 +7,8 @@ namespace PlatformBuild.CmdLineProxies
 	{
 		public int Build(FilePath buildPath)
 		{
-			var executablePath = buildPath.Append((FilePath) "Build.cmd").ToEnvironmentalPath();
-			
+			var executablePath = buildPath.Append((FilePath)"Build.cmd").ToEnvironmentalPath();
+
 			if (File.Exists(executablePath))
 				return buildPath.CallInFolder("Build.cmd", "");
 
@@ -18,19 +18,27 @@ namespace PlatformBuild.CmdLineProxies
 
 		public int RunSqlScripts(FilePath root, FilePath script)
 		{
-            // todo: make this configurable:
-            var buildPath = root.Navigate((FilePath)"build");
-            var sqlInfo = buildPath.Navigate((FilePath)"sql.rule").ToEnvironmentalPath();
+			/* Old, platform neutral way
+			// todo: make this configurable:
+			var buildPath = root.Navigate((FilePath)"build");
+			var sqlInfo = buildPath.Navigate((FilePath)"sql.rule").ToEnvironmentalPath();
 
-            if (!File.Exists(sqlInfo))
-            {
-	            Log.Verbose("*** Must have build/sql.rule file to get parallel sql ***");
-                return 0;
-            }
+			if (!File.Exists(sqlInfo))
+			{
+				Log.Verbose("*** Must have build/sql.rule file to get parallel sql ***");
+				return 0;
+			}
 
 			var sqlParams = File.ReadAllText(sqlInfo).Trim();
 			Log.Status("                           " + script.LastElement());
 			return buildPath.CallInFolder("sqlfk.exe", sqlParams + " -i \"" + script.ToEnvironmentalPath() + "\"");
+			*/
+
+			//sqlcmd.exe -i \"#{script}\" -S #{server} -E -o #{temp_log}
+
+			// quick and dirty hack way:
+			Log.Status("                           " + script.LastElement());
+			return root.CallInFolder("sqlcmd.exe", " -i \"" + script.ToEnvironmentalPath() + "\" -S .\\\\SQLEXPRESS -E");
 		}
 	}
 }
